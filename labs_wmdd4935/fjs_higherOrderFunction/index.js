d20a6d1ff559890335282e4d1cfe684032969e65
Kunal Verma <kunalv508@gmail.com>
2017-12-04 09:33:03 -0800

    Add files via upload

---
+++ labs_wmdd4935/fjs_higherOrderFunction/index.js
@@ -0,0 +1,8 @@
+function repeat(operation, num) {
+      // SOLUTION GOES HERE
+      for(let i = 0; i < num ; i++)
+        operation;
+}   
+
+// Do not remove the line below
+module.exports = repeat
